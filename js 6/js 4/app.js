6f143e0e7479dae42e696c58e015b983be0d9e99
Hammadkhan95 <145599779+Hammadkhan95@users.noreply.github.com>
2023-10-05 00:03:15 +0500

    Add files via upload

---
+++ js 6/js 4/app.js
@@ -0,0 +1,136 @@
+// Q: no 1
+
+for (let i = 0; i < 10; i++)
+  
+//   Q: no 2
+
+  for (let i = 0; i <= 12; i++)
+
+//   Q:no 3
+
+for (var i = 0; i <= 4; i++)
+
+// Q: no 4
+  
+for (let j = 0; j < 100; j++) 
+
+// Q:no 5
+
+for (let i = 3; i > 0; i--) 
+
+// Q: no 6
+
+let arrayLength = pets.length;
+
+
+// Q : no 7
+  
+let flag = true; 
+
+
+// Q : no 8
+
+for (let i = 0; i < pets.length; i++) 
+ 
+
+// Q : no 9
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 2) {
+      alert("The counter value is: " + (i - 1));
+      break;
+    }
+  }
+  
+
+// Q : no 10
+
+/
+var userNames = ["John", "Alice", "Bob", "Eve"];
+
+
+var firstName = prompt("Enter first name");
+
+
+for (var i = 0; i < userNames.length; i++) 
+
+
+
+// Q : no 11
+
+var matchFound = false;
+
+for (var i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("Match found");
+    matchFound = true;
+    break;
+  }
+}
+
+
+if (!matchFound) {
+  alert("Match not found");
+}
+
+
+// Q : no 12
+
+for (let i = 1; i <= 10; i++) {
+   
+    alert("Counter value is: " + (i - 1)); ration.
+    break; 
+  }
+  
+  
+  var userNames = ["John", "Alice", "Bob", "Eve"];
+  
+ 
+  var firstName = prompt("Enter first name");
+
+  
+  for (var i = 0; i < userNames.length; i++) {
+   
+    if (firstName === userNames[i]) {
+      alert("Enter");
+    } else {
+      alert("Please write correct user name"); 
+  }
+  
+  
+  var matchFound = false;
+  for (var i = 0; i < userNames.length; i++) {
+    if (firstName === userNames[i]) {
+      alert("Match found");
+      matchFound = true;
+      break;
+    }
+  }
+  if (!matchFound) {
+    alert("Match not found"); 
+  }
+
+  var firstArr = ["a", "b", "c", "d", "e", "f"];
+  var secondArr = [1, 2, 3, 4, 5, 6];
+  
+ 
+  for (var i = 0; i < firstArr.length; i++) {
+   
+    for (var j = 0; j < secondArr.length; j++) {
+      
+      console.log(firstArr[i] + secondArr[j]);
+    }
+  }
+  
+
+
+
+
+
+  
+
+
+
+
+
+
