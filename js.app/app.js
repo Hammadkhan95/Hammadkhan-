c3fc71b9baee5001243190797f07def5b6cf829b
Hammadkhan95 <145599779+Hammadkhan95@users.noreply.github.com>
2023-10-04 21:46:13 +0500

    Add files via upload

---
+++ js.app/app.js
@@ -0,0 +1,93 @@
+
+// alert("First Name: Hammad ");
+// alert("Last Name: khan");
+// alert("Email: hammadkhan95h@gamil.com");
+// alert("Phone Number: 03453827939");
+alert("You're learning JavaScript!");
+
+alert("Hello, World! This is an alert message.");
+var myVariableName = "This is a variable in camelCase";
+
+var myVariable = "This is a string value assigned to myVariable"; 
+var teamName = "Hammad"; 
+alert("Team Name: " + teamName); 
+
+var bestMan = "Charlie";
+bestMan = "Hammad"; 
+
+var caseQty;
+
+
+var caseQty;
+caseQty = 144;\
+
+var num = parseInt("9");
+
+var result; 
+result = 5 + 7; 
+
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+
+var myNumber = 5; 
+myNumber = myNumber + 3; 
+
+var productCost = 3.45;
+
+var nameOfBand;
+
+var myNumber = 42;
+
+var myFullName = "Hammad";
+
+Legal Variable Names:
+
+1.Variable names must start with a letter (a-z, A-Z) or an underscore (_) character.
+
+Legal: myVar, _privateVar
+Illegal: 123Var, @specialVar
+2.After the first character, variable names can contain letters, digits (0-9), underscores (_), and dollar signs ($).
+
+Legal: myVar2, _var_name, $price
+Illegal: my-var, my var, my%var
+
+3.Variable names are case-sensitive, so myVar and myvar are treated as different variables.
+
+Legal: myVar, myvar
+Illegal: None
+
+
+In JavaScript, the name of the arithmetic operator that gives you the remainder when one number is divided by another is called the "modulo" operator. The symbol for the modulo operator is %.
+
+
+In JavaScript, when you use the modulo operator %, it calculates the remainder when one number is divided by another. In the code var num = 20 % 6;, num will be assigned the value 2.
+
+This is because when you divide 20 by 6, you get a quotient of 3 with a remainder of 2, and the modulo operator % returns the remainder, which is 2 in this case.
+
+
+var largeNum = 1000 * 2000;
+
+
+var result = variable1 - variable2;
+var myRemainder = 10 % 3; 
+
+var num1 = 5; 
+var num2 = 3; 
+var result = num1 * num2;
+
+alert("The result of the multiplication is: " + result);
+
+
+
+
+
+
+
+
+
+
+
+
+
